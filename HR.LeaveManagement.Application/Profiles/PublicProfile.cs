b57564eac064ed9654b3b78da6e50e58936f33b6
Lakshya Purohit <lakshya.purohit.2105@gmail.com>
2024-07-27 02:13:24 +0530

    Add project files.

---
+++ HR.LeaveManagement.Application/Profiles/PublicProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    internal class PublicProfile :Profile
+    {
+        public PublicProfile()
+        {
+            CreateMap<LeaveType,LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveRequest,LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveAllocation,LeaveAllocationDto>().ReverseMap();   
+        }
+    }
+}
---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Domain.common;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        public string Name { get; set; }
+
+        public int DefaultDays { get; set; }
+    }
+}
